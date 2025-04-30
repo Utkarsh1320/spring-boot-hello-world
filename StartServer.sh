6e8ceac87ce0af015170df944da94e626c30eadc
Utkarsh More <77724790+Utkarsh1320@users.noreply.github.com>
2025-04-30 12:55:33 -0400

    Update StartServer.sh

--- StartServer.sh
+++ StartServer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the directory where your application is deployed
-cd /home/ec2-user/deploy
+cd /home/ec2-user
 
 # Stop the currently running Spring Boot application (if any)
 echo "Stopping the existing Spring Boot application..."
@@ -9,9 +9,9 @@ sudo pkill -f 'java -jar springboot-hello-world.jar'
 
 # Start the new Spring Boot application
 echo "Starting the new Spring Boot application..."
-nohup java -jar springboot-hello-world.jar > /home/ec2-user/deploy/app.log 2>&1 &
+nohup java -jar springboot-hello-world.jar > /home/ec2-user/app.log 2>&1 &
 
 # Optionally, monitor the application log for errors or issues
-tail -f /home/ec2-user/deploy/app.log
+tail -f /home/ec2-user/app
 
 echo "Deployment completed successfully!"
