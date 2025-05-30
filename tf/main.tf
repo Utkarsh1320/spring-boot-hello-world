3934027935be6ae724e6d74f48a09a4d5bfdb45c
Utkarsh More <utkarsh.more.2000@gmail.com>
2025-05-29 23:42:58 -0400

    backend added

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "kubernetes-project-pipeline"
+    key    = "tf/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
